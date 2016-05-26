035d0a7b9e94b9dc6f777ce6fe51ec3899195089
Melissa Canales <missmeliss621@gmail.com>
2016-05-26 09:59:42 -0500

    With this iteration I removed the php functionality. This will be added back in as a group, at a later time.

---
+++ public/templates/log_in.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Adlister Project</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <form class="form-horizontal" method="POST" action="login.php">
+      <div class="form-group form-group-lg">
+        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+        <div class="col-sm-10">
+          <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+        </div>
+      </div>
+      <div class="form-group form-group-lg">
+        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+        </div>
+      </div>
+      <div class="form-group form-group-lg">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-default">Log in</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
