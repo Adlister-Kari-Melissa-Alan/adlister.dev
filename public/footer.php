29dd70f814bb461bf476a9cb253778d39de453ce
Melissa Canales <missmeliss621@gmail.com>
2016-05-26 08:25:31 -0500

    I updated the existing html to bootstrap. This isncluded adding CSS that will need to be merged into the master at the pre-designated time.

--- public/footer.php
+++ public/footer.php
@@ -1,7 +1,33 @@
-<footer id="XXXXX">
-  <ul>
-    <li>&copy; 2016 
-    <li><a href="#footer-Alan">Alan Lauritzen</a></li>
-    <li><a href="#footer-Kari">Kari Palomino</a></li>
-    <li><a href="#footer-Melissa">Melissa Canales</a></li>
+<style>
+html {
+  position: relative;
+  min-height: 100%;
+}
+body {
+  /* Margin bottom by footer height */
+  margin-bottom: 60px;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height: 60px;
+  background-color: #f5f5f5;
+}
+footer li {
+    list-style-type: none;
+    display: inline-block;
+}
+</style>
+
+<footer class="footer">
+    <div class="container">
+        <ul>
+            <li>&copy; 2016 </li>
+            <li><a href="#footer-Alan">Alan Lauritzen</a></li>
+            <li><a href="#footer-Kari">Kari Palomino</a></li>
+            <li><a href="#footer-Melissa">Melissa Canales</a></li>
+        </ul>      
+  </div>
 </footer>
