baa84ce0dab18fd9e4ab3c6620164b0f0d5badd3
Alan <alaurit@gmail.com>
2016-05-26 09:59:58 -0500

    initial commit user_profile page

---
+++ public/templates/user_profile.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User profile</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<style>
+
+    .user_profile {
+        margin-bottom: 60px;
+    }
+
+    .user_profile_container {
+        width: 25em;
+    }
+
+</style>
+</head>
+<body>
+    <div class="container user_profile_container" >
+        <div class="user_profile">
+            <p class"profile_head">Your info</p>
+            <dl class="well">
+                <dt>Name</dt>
+                <dd> # </dd>
+
+                <dt>Email</dt>
+                <dd> # </dd>
+            </dl>
+            <a class="btn btn-default" href="#" role="button">Edit Profile</a>
+        </div>
+
+        <div class="user_profile">
+            <p class"profile_head">Your ads</p>
+            <ul class="well">
+                <li>Ad 1</li>
+                <li>Ad 2</li>
+            </ul>
+            <a class="btn btn-default" href="#" role="button">Create Ad</a>
+        </div>
+    </div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
