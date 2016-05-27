59cbe01d5e558ee61a45e3c3d7821d4bd4d385fc
Melissa Canales <missmeliss621@gmail.com>
2016-05-27 14:47:53 -0500

    This iteration included consolidating the styles in the main.css as well as a few bootstrap overrides.

--- public/templates/index.php
+++ public/templates/index.php
@@ -24,70 +24,49 @@ extract(fetchThree($dbc));
 <head>
     <title>Adlister Project</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-    <style>
-    .item img {
-        margin-left: auto;
-        margin-right: auto;
-        max-height: 450px;
-    }
-
-    .carousel-caption {
-        color: black;
-        bottom: -80px;
-        text-shadow: none;
-    }
-
-    .carousel-title {
-
-    }
-
-    .carousel-inner {
-        overflow: visible;
-    }
-
-    </style>
+    <!-- make sure this path is changed after the index is moved out of templates -->
+    <link rel="stylesheet" type="text/css" href="../css/main.css">
 
 </head>
 <body>
+<?php require_once "../../views/partials/navbar.php" ?>
+<div id="main_container">
     <div id="carousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel" data-slide-to="1"></li>
-            <li data-target="#carousel" data-slide-to="2"></li>
-        </ol>
-
-  <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-
-        <?php foreach ($array as $ad) {
-            if ($ad == $array[0]) { ?>
-                <div class="item active">
-                <?php } else { ?>
-                    <div class="item">
-            <?php } ?>
-            <img src="../<?= $ad['image_url'] ?>" alt="<?= $ad['name'] ?>">
-            <div class="carousel-caption">
-                <p class='carousel-title'><?= $ad['name'] ?></p>
-            </div>
-        </div>
-
-        <?php }; ?> <!-- end foreach -->
-
+        <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel" data-slide-to="1"></li>
+                <li data-target="#carousel" data-slide-to="2"></li>
+            </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <?php foreach ($array as $ad) {
+                if ($ad == $array[0]) { ?>
+                    <div class="item active">
+                        <?php } else { ?>
+                            <div class="item"><?php } ?>
+                                <img src="../<?= $ad['image_url'] ?>" alt="<?= $ad['name'] ?>">
+                                    <div class="carousel-caption">
+                                        <p class='carousel-title'><?= $ad['name'] ?></p>
+                                    </div>
+                            </div>
+                            <?php }; ?> <!-- end foreach -->
+                    </div>
+        </div>  
+      <!-- Controls -->
+        <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-
-  <!-- Controls -->
-    <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
 </div>
-
+<div id="footer-container">
+    <?php require_once "../footer.php" ?>
+</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
