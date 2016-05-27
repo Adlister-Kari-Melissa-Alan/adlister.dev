c05c5e8fef2b6fcdf498dc1deefa74c3ae39fa6c
Alan <alaurit@gmail.com>
2016-05-27 15:32:14 -0500

    copied from templates to views

--- views/home.php
+++ views/home.php
@@ -1,27 +1,54 @@
-<div class="container">
+<?php
+// require_once '../models/Model.php';
+require_once '../database/db_connect.php';
+    
 
-    <section id="welcome">
+function fetchThree($dbc) {
 
-        <div class="row">
+    $query=
+        'SELECT * FROM 
+        (SELECT * FROM ads ORDER BY id DESC LIMIT 0 , 3) AS descended
+        ORDER BY id ASC;';
+    $carouselItems = [];
+    $carouselItems['array']=$dbc->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($carouselItems);
+    return $carouselItems;
+}
 
-            <div class="col-xs-12">
+extract(fetchThree($dbc));
+?>
 
-                <h1 class="text-center">Welcome To Adlister</h1>
-
-            </div>
-
-        </div>
-
-    </section>
-
-    <section id="features">
-
-        <div class="row">
-
-            <h3 class="section-title">Featured Items</h3>
-            <!-- Placeholder for featured items.-->
-        </div>
-
-    </section>
-
+<div id="main_container">
+    <div id="carousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel" data-slide-to="1"></li>
+                <li data-target="#carousel" data-slide-to="2"></li>
+            </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <?php foreach ($array as $ad) {
+                if ($ad == $array[0]) { ?>
+                    <div class="item active">
+                        <?php } else { ?>
+                            <div class="item"><?php } ?>
+                                <img src="../<?= $ad['image_url'] ?>" alt="<?= $ad['name'] ?>">
+                                    <div class="carousel-caption">
+                                        <p class='carousel-title'><?= $ad['name'] ?></p>
+                                    </div>
+                            </div>
+                            <?php }; ?> <!-- end foreach -->
+                    </div>
+        </div>  
+      <!-- Controls -->
+        <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 </div>
--- views/users/account.php
+++ views/users/account.php
@@ -1,1 +1,68 @@
-<!--Page for user account home-->
+<?php
+// require_once '../../utils/Auth.php';
+
+$user=getUser("id");
+// var_dump($user);
+
+function getUser($id, $default=null) {
+    if (isset($_REQUEST[$id])) {
+        return $_REQUEST[$id];
+        } else {
+            return $default;
+        }
+}
+
+
+$infoQuery=
+    "SELECT name, email FROM users WHERE id={$user};";
+$usersInfo = [];
+$usersInfo['array']=$dbc->query($infoQuery)->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($usersInfo['array']);
+
+
+function getUsersAds($dbc, $user) {
+    $query=
+        "SELECT ads.id, ads.name, users.name AS user_name, users.email FROM ads JOIN users ON ads.user_id = users.id WHERE users.id={$user};";
+    $usersAds = [];
+    $usersAds['array']=$dbc->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($usersAds);
+    return $usersAds;
+}
+
+extract(getUsersAds($dbc, $user));
+
+?>
+
+<div class="container user_profile_container" >
+    <div class="user_profile">
+        <p class"profile_head">Your info</p>
+        <dl class="well">
+            <dt>Name</dt>
+            <dd> <?= $usersInfo['array'][0]['name'] ?> </dd>
+
+            <dt>Email</dt>
+            <dd> <?= $usersInfo['array'][0]['email'] ?> </dd>
+        </dl>
+
+        <?php 
+            if (Auth::check()) { ?>
+                <a class="btn btn-default" href="#" role="button">Edit Profile</a>
+            <?php } ?>
+    </div>
+
+    <div class="user_profile">
+        <p class"profile_head">Your ads</p>
+        <ul class="well list-unstyled">
+            
+        <?php foreach ($array as $ad) { ?>
+
+        <li><?= substr($ad['name'], 0, 25) ?>...</li>
+
+    <?php }; ?> <!-- end foreach -->
+        </ul>
+        <?php 
+            if (Auth::check()) { ?>
+                <a class="btn btn-default" href="ads/create.php" role="button">Create Ad</a>
+            <?php } ?>
+    </div>
+</div>
