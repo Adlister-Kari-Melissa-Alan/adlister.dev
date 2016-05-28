6bfb8f0471dd29c2b1019cbde3b66e816a6e5839
Alan <alaurit@gmail.com>
2016-05-28 03:31:57 -0500

    fixed logout page

--- public/logout.php
+++ public/logout.php
@@ -1,21 +1,10 @@
 <?php
 require_once '../utils/Auth.php';
-
-    // clear $_SESSION array
-    session_unset();
-
-    // session_destroy();
-
-    // delete session data on the server and send the client a new cookie
-    session_regenerate_id(true);
-
-
-
-    // Auth::logout();
+    session_start();
     
+    Auth::logout();
 
     header("Location: /");
     exit;
-    // var_dump(Auth::logout());
-
+    
 ?>
