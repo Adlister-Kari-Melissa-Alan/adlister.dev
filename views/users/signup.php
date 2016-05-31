155346fb6406f8775a3a08f935c8584db47b4dc0
Melissa Canales <missmeliss621@gmail.com>
2016-05-31 10:09:18 -0500

    On this iteration, I made a few tweaks to the css that resulted in an additional div container.

--- views/users/signup.php
+++ views/users/signup.php
@@ -26,18 +26,20 @@ if($_SERVER['REQUEST_METHOD']==='POST') {
 ?>
 
 <div class="container">
-        <h1>Sign up for free!</h1>
-        <form action="/users/signup" method="POST">
-            <div class="form-group form-group-lg">
-            <input type="text" class="form-control" placeholder="NAME" name="name"></div>
-            <div class="form-group form-group-lg">
-            <input type="text" class="form-control" placeholder="USERNAME" name="username"></div>
-            <div class="form-group form-group-lg">
-            <input type="email" class="form-control" placeholder="EMAIL" name="email"></div>
-            <div class="form-group form-group-lg">
-            <input type="password" class="form-control" placeholder="PASSWORD" name="password"></div>
-            <div class="form-group form-group-lg">
-            <input type="password" class="form-control" placeholder="CONFIRM PASSWORD" name="confirm"></div>
-            <button type="submit" class="btn btbn-default" name="sumbit">Submit</button>
-        </form>
-    </div>
+    <h1>Sign up for free!</h1>
+        <div class="signinForm">
+            <form action="/users/signup" method="POST">
+                <div class="form-group form-group-lg">
+                <input type="text" class="form-control" placeholder="NAME" name="name"></div>
+                <div class="form-group form-group-lg">
+                <input type="text" class="form-control" placeholder="USERNAME" name="username"></div>
+                <div class="form-group form-group-lg">
+                <input type="email" class="form-control" placeholder="EMAIL" name="email"></div>
+                <div class="form-group form-group-lg">
+                <input type="password" class="form-control" placeholder="PASSWORD" name="password"></div>
+                <div class="form-group form-group-lg">
+                <input type="password" class="form-control" placeholder="CONFIRM PASSWORD" name="confirm"></div>
+                <button type="submit" class="btn btbn-default" name="sumbit">Submit</button>
+            </form>
+        </div>
+</div>
